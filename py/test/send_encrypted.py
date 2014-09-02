e60c3893b9b536a83afc1c4685c01d42161cd28c
ortutay <marcell.ortutay@gmail.com>
2014-09-02 02:15:14 -0700

    rm files from other repo

--- py/test/send_encrypted.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import sys
-from bitnet_client import BitnetClient
-
-a = BitnetClient(".bitnet3")
-a.SendEncrypted(sys.argv[1], sys.argv[2])
--- py/test/send_to_pubkey.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import sys
-from bitnet_client import BitnetClient
-
-a = BitnetClient(".bitnet2")
-a.Send(sys.argv[1], sys.argv[2])
-
